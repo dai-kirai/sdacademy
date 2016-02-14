3415d8505c245a4c5e95ea135b9a431055ecdabd
Daikirai <maksym.v.rodionov@gmail.com>
2016-02-15 01:52:15 +0200

    Should be ok

--- quadratic/views.py
+++ quadratic/views.py
@@ -3,13 +3,37 @@ from django.http import HttpRequest
 from django.core.urlresolvers import reverse
 
 
-def results(request):
-    a = int(request.GET['a'])
-    b = int(request.GET['b'])
-    c = int(request.GET['c'])
+def check_int(string):
+    global notint
+    try:
+        return int(string)
+    except ValueError:
+        notint = True
 
-    d = b ** 2 - 4 * a * c
 
-    return render(request, 'results.html')
+def quadratic_results(request):
+    global notint
+    notint = False
+    x1 = 0
+    x2 = 0
+    a = request.GET.get(u'a', '')
+    b = request.GET.get(u'b', '')
+    c = request.GET.get(u'c', '')
+
+    a1 = check_int(a)
+    b1 = check_int(b)
+    c1 = check_int(c)
+
+    context = {'a': a, 'b': b, 'c': c, 'notint': notint}
+
+    if not notint and (a1 != 0):
+        d = b1 ** 2 - 4 * a1 * c1
+        if d >= 0:
+            x1 = (-b1 - d ** (1/2.0)) / (2 * a1)
+            x2 = (-b1 - d ** (1/2.0)) / (2 * a1)
+        context.update({'d': d, 'x1': x1, 'x2': x2})
+
+    return render(request, 'results.html', context)
+
 
 # Create your views here.
