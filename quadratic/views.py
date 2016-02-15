534403779cc061488cfa7a3878ef0dd4efa01f6c
Daikirai <maksym.v.rodionov@gmail.com>
2016-02-15 17:25:40 +0200

    x2 fixed

--- quadratic/views.py
+++ quadratic/views.py
@@ -30,7 +30,7 @@ def quadratic_results(request):
         d = b1 ** 2 - 4 * a1 * c1
         if d >= 0:
             x1 = (-b1 - d ** (1/2.0)) / (2 * a1)
-            x2 = (-b1 - d ** (1/2.0)) / (2 * a1)
+            x2 = (-b1 + d ** (1/2.0)) / (2 * a1)
         context.update({'d': d, 'x1': x1, 'x2': x2})
 
     return render(request, 'results.html', context)
